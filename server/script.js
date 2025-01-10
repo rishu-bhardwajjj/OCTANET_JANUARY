57a786469858d2d549a73f6ee4a6a27b00e2fd9a
RIshabh Bhardwaj <120660474+rishu-bhardwajjj@users.noreply.github.com>
2025-01-10 19:21:50 +0530

    Initial Commit

---
+++ server/script.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve your main HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'server/public', 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
